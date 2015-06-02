12a0f60066d9da06d220025b2201cb59920d6fd3
Tom Carmona <thomascarmona@gmail.com>
2015-06-02 14:48:42 -0500

    initial commit

---
+++ GameOfThrones/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfThrones
+//
+//  Created by Tom Carmona on 6/2/15.
+//  Copyright (c) 2015 Tom Carmona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
